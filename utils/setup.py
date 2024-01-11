4e0abcf9493194df9a6786e40d45b6474367b4ba
Navaneet Krishna <navaneetkrishna@N0071-Navaneet.local>
2024-01-11 22:54:56 +0530

    sqlite

--- utils/setup.py
+++ utils/setup.py
@@ -6,15 +6,11 @@ from dotenv import load_dotenv
 
 @st.cache_resource(ttl=3600)
 def setup_connexion():
-    if "vanna_api_key" in st.secrets and "host" in st.secrets:
+    if "vanna_api_key" in st.secrets and "url" in st.secrets:
         vn.set_api_key(st.secrets.get("vanna_api_key"))
         vn.set_model("vanna-increff-demo")
-        vn.connect_to_postgres(
-            host=st.secrets.get("host"),
-            dbname=st.secrets.get("dbname"),
-            user=st.secrets.get("user"),
-            password=st.secrets.get("password"),
-            port=st.secrets.get("port"),
+        vn.connect_to_sqlite(
+            host=st.secrets.get("url"),
 
         )
 
@@ -22,12 +18,8 @@ def setup_connexion():
         load_dotenv()
         vn.set_api_key(os.environ.get("VANNA_API_KEY"))
         vn.set_model("vanna-increff-demo")
-        vn.connect_to_postgres(
-            host=os.environ.get("host"),
-            dbname=os.environ.get("dbname"),
-            user=os.environ.get("user"),
-            password=os.environ.get("password"),
-            port=os.environ.get("port"),
+        vn.connect_to_sqlite(
+            url=os.environ.get("url"),
         )
 
 
